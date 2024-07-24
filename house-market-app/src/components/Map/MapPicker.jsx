2a7b81d00d9a83997cc2a2cefdf2de303c1c3f21
pouyabakhshi <pouyabakhshiinfo@gmail.com>
2024-07-24 12:43:14 +0330

    modify MapPicker Component (debug Marker Problem)

--- house-market-app/src/components/Map/MapPicker.jsx
+++ house-market-app/src/components/Map/MapPicker.jsx
@@ -1,27 +1,28 @@
-import React, { useCallback } from "react";
-import { useTheme } from "../../contexts/ThemeContext";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { useCallback, useEffect } from "react";
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const setDefaultIcon = () => {
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+  });
+};
 
 const LocationPicker = ({ setLat, setLng, setAddress, location }) => {
-  const map = useMapEvents({
-    click(e) {
-      const { lat, lng } = e.latlng;
-      setLat(lat);
-      setLng(lng);
-      fetchAddress(lat, lng);
-    },
-  });
-
   const fetchAddress = useCallback(
     async (lat, lng) => {
       try {
         const response = await fetch(
           `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&addressdetails=1`,
           {
-            headers: {
-              "Accept-Language": "fa", // request Persian address
-            },
+            headers: { "Accept-Language": "fa" }, // request Persian address
           }
         );
         const data = await response.json();
@@ -34,19 +35,28 @@ const LocationPicker = ({ setLat, setLng, setAddress, location }) => {
     [setAddress]
   );
 
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      setLat(lat);
+      setLng(lng);
+      fetchAddress(lat, lng);
+    },
+  });
+
   return location && <Marker position={[location.lat, location.lng]} />;
 };
 
 const MapPicker = ({ location, setLat, setLng, setAddress }) => {
-  const { theme } = useTheme();
-  const mapClass = theme === "dark" ? "map-dark" : "map-light";
+  useEffect(() => {
+    setDefaultIcon();
+  }, []);
 
   return (
     <MapContainer
       center={location ? [location.lat, location.lng] : [35.7219, 51.3347]}
       zoom={10}
       style={{ height: "400px", width: "100%" }}
-      className={mapClass}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
